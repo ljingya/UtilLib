5b52e83f7c80786bbf59a9b45237ce6fdc81de32
jybeyond <ljya0805@163.com>
2018-12-03 15:32:09 +0800

    添加网络相关的类

---
+++ utillib/src/main/java/com/lijingya/util/NetworkUtils.java
@@ -0,0 +1,12 @@
+package com.lijingya.util;
+
+/**
+ * @author lijingya
+ * @description 添加描述
+ * @email lijingya@91118.com
+ * @createDate 2018/12/3
+ * @company 杭州天音
+ */
+public class NetworkUtils {
+
+}
