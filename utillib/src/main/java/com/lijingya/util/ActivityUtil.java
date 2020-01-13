a1594929d49059f6215c3ba5cc7f84652185f86e
ljingya <ljya0805@163.com>
2020-01-13 17:13:17 +0800

    添加activity工具

--- utillib/src/main/java/com/lijingya/util/ActivityUtil.java
+++ utillib/src/main/java/com/lijingya/util/ActivityUtil.java
@@ -1,12 +1,32 @@
 package com.lijingya.util;
 
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+
 /**
  * @author Administrator
- * @description 添加描述
+ * @description Activity相关的工具类
  * @email Administrator@91118.com
  * @createDate 2018/12/6
  * @company 杭州天音
  */
 public class ActivityUtil {
+
+    /**
+     * 获取该Actviity的任务栈名称
+     * @param activity
+     * @return
+     */
+    public static String getActivityTaskAffinity(Activity activity){
+        try {
+            ActivityInfo activityInfo= activity.getPackageManager().getActivityInfo(activity.getComponentName(), PackageManager.GET_META_DATA);
+       return activityInfo.taskAffinity;
+        } catch (NameNotFoundException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
 
 }
