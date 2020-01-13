c6cf242f0fa996e02280cb6d696a000f48cec2ae
ljingya <ljya0805@163.com>
2020-01-13 17:11:46 +0800

    添加activity工具

---
+++ utillib/src/main/java/com/lijingya/util/ActivityUtil.java
@@ -0,0 +1,12 @@
+package com.lijingya.util;
+
+/**
+ * @author Administrator
+ * @description 添加描述
+ * @email Administrator@91118.com
+ * @createDate 2018/12/6
+ * @company 杭州天音
+ */
+public class ActivityUtil {
+
+}
